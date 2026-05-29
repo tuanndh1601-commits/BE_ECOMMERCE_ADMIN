2c121aee8b03b1628696a5b9410b87864d240063
tuanndh1601 <mylove16146@gmail.com>
2026-05-30 00:46:33 +0700

    Dựng base

--- ECOM.API/Program.cs
+++ ECOM.API/Program.cs
@@ -2,70 +2,88 @@ using ECOM.APPLICATION;
 using ECOM.INFRASTRUCTURE;
 using ECOM.INFRASTRUCTURE.Persistence.Dapper;
 using ECOM.INFRASTRUCTURE.Persistence.EntityFramework;
-using Scalar.AspNetCore;
-using Microsoft.EntityFrameworkCore;
 using ECOM.SHARED.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ======================
-// 🔧 Add services
-// ======================
+// =========================
+// SERVICES
+// =========================
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
-// 👉 Auto-inject services
+// Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ECOM API",
+        Version = "v1",
+        Description = "ECOM Backend API"
+    });
+});
+
+// Auto DI
 builder.Services.AddAutoDI(
     typeof(ApplicationAssembly).Assembly,
     typeof(InfrastructureAssembly).Assembly
 );
 
+// Dapper
 builder.Services.AddScoped<IDbConnectionFactory, SqlConnectionFactory>();
 
 builder.Services.AddScoped<IDapperRepository, DapperRepository>();
 
-
+// EF
 builder.Services.AddDbContext<BaseDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default"));
 });
-
-builder.Services.AddOpenApi();
-// ======================
-// 🏗️ Build app
-// ======================
 
 var app = builder.Build();
 
-// ======================
-// 🌐 Middleware
-// ======================
-
-// 2. Kích hoạt Middleware (Phải nằm TRƯỚC app.Run)
-if (app.Environment.IsDevelopment())
-{
-
-}
-
-app.MapOpenApi();
-
-app.MapScalarApiReference(options =>
-{
-    options
-        .WithTitle("ECOM API")
-        .WithTheme(ScalarTheme.DeepSpace)
-        .WithDefaultHttpClient(
-            ScalarTarget.CSharp,
-            ScalarClient.HttpClient);
-});
+// =========================
+// MIDDLEWARE
+// =========================
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Swagger
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    // Title trình duyệt
+    options.DocumentTitle = "ECOM API";
+
+    // Collapse endpoint
+    options.DocExpansion(DocExpansion.None);
+
+    // Ẩn schema/model
+    options.DefaultModelsExpandDepth(-1);
+
+    // Hiển thị thời gian request
+    options.DisplayRequestDuration();
+
+    // Search API
+    options.EnableFilter();
+
+    // KHÔNG set RoutePrefix
+    // => dùng mặc định /swagger/index.html
+
+    options.SwaggerEndpoint(
+        "/swagger/v1/swagger.json",
+        "ECOM API v1");
+});
 
 app.Run();
